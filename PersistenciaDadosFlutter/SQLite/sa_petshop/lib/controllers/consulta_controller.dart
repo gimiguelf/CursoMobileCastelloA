ab29d03b290a1661ee9caa46c331c02f0d70798f
gimiguelf <miguelgiovana407@gmail.com>
2025-06-03 16:57:01 -0300

    03.06.2025

---
+++ PersistenciaDadosFlutter/SQLite/sa_petshop/lib/controllers/consulta_controller.dart
@@ -0,0 +1,20 @@
+import 'package:sa_petshop/models/consulta_model.dart';
+import 'package:sa_petshop/services/db_helper.dart';
+
+class ConsultaController {
+  final _dbHelper = DbHelper();
+
+  // métodos do controller
+  Future<int> createConsulta(Consulta consulta) async {
+    return _dbHelper.insertConsulta(consulta);
+  }
+
+  Future<List<Consulta>> readConsultaByPet(int petId) async {
+    return _dbHelper.getConsultaByPet(petId);
+  }
+  
+  Future<int> deleteConsulta(int id) async {
+    return _dbHelper.deletConsulta(id);
+  }
+
+}
